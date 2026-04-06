831aaddf2cd771c150a18fae9081a1b18fc556e1
Mrinmoy1024 <mtex1024@gmail.com>
2026-04-07 00:17:26 +0600

    added necesarry packages

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173/"
+    ]
+  })
+)
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
